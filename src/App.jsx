8d7b9307e82dcc71ff183519c1fefa7bd49b405e
Brunno Miguel <miguel.brunno06@gmail.com>
2022-06-21 13:34:49 -0300

    feat: UserContext for token validation; react-toastify for api request responses;

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,24 @@
 import GlobalStyle from "./styles/global";
 
+import "react-toastify/dist/ReactToastify.css";
+import { ToastContainer } from "react-toastify";
+
 const App = () => {
   return (
-    <div>
+    <>
       <GlobalStyle />
-    </div>
+      <ToastContainer
+        position="top-right"
+        autoClose={4000}
+        hideProgressBar={false}
+        newestOnTop={false}
+        closeOnClick
+        rtl={false}
+        pauseOnFocusLoss
+        draggable
+        pauseOnHover
+      />
+    </>
   );
 };
 

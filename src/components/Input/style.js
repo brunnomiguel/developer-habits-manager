155cf574d4f23a563120b3367b9519ca795605db
Eric James <35430109+Ericjcf@users.noreply.github.com>
2022-06-22 10:12:23 -0300

    fix: css

--- src/components/Input/style.js
+++ src/components/Input/style.js
@@ -3,16 +3,14 @@ import styled, {css} from "styled-components";
 export const Container = styled.div`
   margin: 10px 0 10px 0;
   text-align: left;
-  width: 280px;
+  width: 100%;
   height: 80px;
 
-  @media (max-width: 720px) {
-    width: 410px;
-  }
+  
 `;
 
 export const InputContainer = styled.div`
-  background: var(--gray-5);
+  background: var(--white-1);
   border-radius: 5px;
   border: none;
   margin-top: 5px;

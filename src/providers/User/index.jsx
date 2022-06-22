c39cdb73e950ddac01a00495689eeb8a21a7a72d
Brunno Miguel <miguel.brunno06@gmail.com>
2022-06-22 20:14:48 -0300

    update: username included in user context

--- src/providers/User/index.jsx
+++ src/providers/User/index.jsx
@@ -1,4 +1,5 @@
-import { createContext, useState } from "react";
+import jwtDecode from "jwt-decode";
+import { createContext, useEffect, useState } from "react";
 import { useHistory } from "react-router-dom";
 
 import { toast } from "react-toastify";
@@ -8,9 +9,8 @@ import api from "../../services/api";
 export const UserContext = createContext();
 
 export const UserProvider = ({ children }) => {
-  const [token, setToken] = useState(
-    localStorage.getItem("@DHM/token") || ""
-  );
+  const [token, setToken] = useState(localStorage.getItem("@DHM/token") || "");
+  const [user, setUser] = useState(localStorage.getItem("DHM/user") || "");
 
   const history = useHistory();
 
@@ -21,14 +21,30 @@ export const UserProvider = ({ children }) => {
         const { access } = res.data;
         setToken(access);
         localStorage.setItem("@DHM/token", access);
-        toast.success("Login realizado com sucesso")
+        toast.success("Login realizado com sucesso");
         history.push("/habits");
       })
       .catch((_) => toast.error("Usuário ou senha inválidos!"));
   };
 
+  const decodeJwt = token && jwtDecode(token);
+
+  async function searchUser() {
+    if (token) {
+      const responseUser = await api.get(`/users/${decodeJwt.user_id}/`);
+      console.log(responseUser.data);
+      const { username } = responseUser.data;
+      setUser(username);
+      localStorage.setItem("DHM/user", username);
+    }
+  }
+
+  useEffect(() => {
+    searchUser();
+  }, [token]);
+
   return (
-    <UserContext.Provider value={{ token, signIn }}>
+    <UserContext.Provider value={{ token, user, signIn }}>
       {children}
     </UserContext.Provider>
   );

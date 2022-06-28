9eb503506572f9299ddc417329785177bc926c7c
Brunno Miguel <miguel.brunno06@gmail.com>
2022-06-28 14:26:33 -0300

    feat: added functions to the goals context

--- src/providers/index.js
+++ src/providers/index.js
@@ -2,13 +2,16 @@ import { UserProvider } from "./User";
 import { ModalProvider } from "./Modal";
 import { HabitsProvider } from "./Habits";
 import { GroupsProvider } from "./Groups";
+import { GoalsProvider } from "./Goals";
 
 const Providers = ({ children }) => {
   return (
     <UserProvider>
       <ModalProvider>
         <HabitsProvider>
-          <GroupsProvider>{children}</GroupsProvider>
+          <GroupsProvider>
+            <GoalsProvider>{children}</GoalsProvider>
+          </GroupsProvider>
         </HabitsProvider>
       </ModalProvider>
     </UserProvider>

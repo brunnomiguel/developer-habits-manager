867595c2e55d4b4c4a1dc863378c8d84519690b2
Brunno Miguel <miguel.brunno06@gmail.com>
2022-06-28 22:47:06 -0300

    feat: function to update activities, activity date formatting.

--- src/providers/Activities/index.jsx
+++ src/providers/Activities/index.jsx
@@ -1,7 +1,8 @@
 import { createContext, useContext, useState } from "react";
+import { UserContext } from "../User";
+
 import { toast } from "react-toastify";
 import api from "../../services/api";
-import { UserContext } from "../User";
 
 export const ActivitiesContext = createContext();
 
@@ -13,21 +14,51 @@ export const ActivitiesProvider = ({ children }) => {
     const responseActivities = await api.get(`/activities/${groupId}/`, {
       headers: { Authorization: `Bearer ${token}` },
     });
-    const dataActivities = responseActivities.data;
+
+    const dataActivities = responseActivities.data.map((activity) => ({
+      ...activity,
+      realization_time: new Date(activity.realization_time).toLocaleDateString(
+        "pt-BR",
+        {
+          day: "2-digit",
+          month: "long",
+          year: "numeric",
+        }
+      ),
+    }));
+
     setActivities(dataActivities);
   }
 
-  // const addNewActivity = (data, groupId) => {
-  //   data.group = groupId;
-  //   api
-  //     .post(`/activities/`, data, {
-  //       headers: { Authorization: `Bearer ${token}` },
-  //     })
-  //     .then((_) => {
-  //       toast.success("Atividade adicionada com sucesso!");
-  //       loadActivities(groupId);
-  //     });
-  // };
+  const addNewActivity = (data, groupId) => {
+    const { realization_time } = data;
+    const date = new Date(realization_time);
+    const isoFormat = date.toISOString();
+
+    data.realization_time = isoFormat;
+    data.group = groupId;
+
+    api
+      .post(`/activities/`, data, {
+        headers: { Authorization: `Bearer ${token}` },
+      })
+      .then((_) => {
+        toast.success("Atividade adicionada com sucesso!");
+        loadActivities(groupId);
+      });
+  };
+
+  const updateActivity = (data, activityId, groupId) => {
+    api
+      .patch(`/activities/${activityId}/`, data, {
+        headers: { Authorization: `Bearer ${token}` },
+      })
+      .then((_) => {
+        toast.success("Atividade atualizada com sucesso!");
+        loadActivities(groupId);
+      })
+      .catch((_) => toast.error("Ops... algo deu errado."));
+  };
 
   const deleteActivity = (activityId, groupId) => {
     api
@@ -42,7 +73,15 @@ export const ActivitiesProvider = ({ children }) => {
   };
 
   return (
-    <ActivitiesContext.Provider value={{ activities, loadActivities, deleteActivity }}>
+    <ActivitiesContext.Provider
+      value={{
+        activities,
+        loadActivities,
+        addNewActivity,
+        updateActivity,
+        deleteActivity,
+      }}
+    >
       {children}
     </ActivitiesContext.Provider>
   );

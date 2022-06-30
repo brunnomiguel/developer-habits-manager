244906a1fc57bc0d6913a8c7affbc3a531b8fb6d
Eduardo Rangel <eduardo_rp23@hotmail.com>
2022-06-30 20:52:25 -0300

    feature: adiciona função para add novo grupo e um sublinhado na página em que o usuário está

--- src/routes/index.jsx
+++ src/routes/index.jsx
@@ -1,33 +1,130 @@
-import { Switch } from "react-router-dom";
-import Route from "./protectedRoute";
+import React, { useContext, useState } from "react";
 
-import { useContext } from "react";
-import { UserContext } from "../providers/User";
+import {
+  Container,
+  CardsContainer,
+  Tittle,
+  PageButtons,
+  InputBttnContainer,
+  AddBttn,
+} from "./styled";
 
-import LandingPage from "../pages/LandingPage";
-import SignUp from "../pages/SignUp";
-import SignIn from "../pages/SignIn";
-import Habits from "../pages/Habits";
-import InitialGroupsPage from "../pages/InitialGroupsPage";
-import YourGroups from "../pages/YourGroups";
-import AllGroups from "../pages/AllGroups";
-import NotFound from "../pages/NotFound";
+import {
+  FiSearch,
+  FiChevronLeft,
+  FiChevronRight,
+  FiPlus,
+} from "react-icons/fi";
 
-const Routes = () => {
-  const { token } = useContext(UserContext);
+import Navbar from "../../components/Navbar";
+import Input from "../../components/Input";
+
+import { LinearProgress, Box } from "@mui/material";
+
+import { GroupsContext } from "../../providers/Groups";
+import Button from "../../components/Button";
+import CardGroup from "../../components/CardGroup";
+import Activities from "../../components/Activities";
+import ModalShowAllGoals from "../../components/ModalShowAllGoals";
+import { ModalContext } from "../../providers/Modal";
+import ModalGroupAddEdit from "../../components/ModalGroupAdd"
+
+const YourGroups = () => {
+  const { groupsSubscribed, loading } = useContext(GroupsContext);
+  const { openAllActivities, openAllGoals, openAddNewGroup, setOpenAddNewGroup  } = useContext(ModalContext);
+
+  const [displayGroup, setDisplayGroup] = useState([]);
+  const [capturedGroup, setCapturedGroup] = useState({});
+  const [inputGroup, setInputGroup] = useState("");
+
+  const searchGroup = (inputGroup) => {
+    inputGroup = inputGroup.toLocaleLowerCase();
+    const filteredGroup = groupsSubscribed.filter((group) => {
+      if (
+        group.name.toLocaleLowerCase().includes(inputGroup) ||
+        group.category.toLocaleLowerCase().includes(inputGroup)
+      ) {
+        return group;
+      }
+    });
+    setDisplayGroup(filteredGroup);
+  };
+
+  const captureGroup = (groupId) => {
+    const verifyGroup = groupsSubscribed.filter(
+      (group) => group.id === groupId
+    );
+    setCapturedGroup(...verifyGroup);
+  };
 
   return (
-    <Switch>
-      <Route exact path="/" component={LandingPage} />
-      <Route path="/SignUp" component={SignUp} />
-      <Route path="/SignIn" component={SignIn} />
-      <Route isPrivate path="/Habits" component={Habits} />
-      <Route isPrivate path="/Groups" component={InitialGroupsPage} />
-      <Route isPrivate path="/YourGroups" component={YourGroups} />
-      <Route isPrivate path="/AllGroups" component={AllGroups} />
-      <Route isPrivate={!!token} component={NotFound} />
-    </Switch>
+    <Container>
+      <Navbar />
+      <Tittle>
+        <h2>seus grupos</h2>
+      </Tittle>
+      <InputBttnContainer>
+        <Input
+          search
+          value={inputGroup}
+          inputHabits={inputGroup}
+          searchHabit={searchGroup}
+          placeholder={"Busque um grupo específico"}
+          onChange={(ev) => {
+            setInputGroup(ev.target.value);
+            searchGroup(ev.target.value);
+          }}
+        >
+          <FiSearch />
+        </Input>
+        <AddBttn>
+          <span>Adicione um novo Gupo</span>
+          <Button white>
+            <FiPlus size={20} onClick={()=>setOpenAddNewGroup(true)}/>
+          </Button>
+        </AddBttn>
+      </InputBttnContainer>
+      <CardsContainer>
+        {loading ? (
+          <Box sx={{ width: "100%", alignSelf: "flex-start" }}>
+            <LinearProgress color="success" />
+          </Box>
+        ) : inputGroup === "" ? (
+          groupsSubscribed?.map((group) => {
+            return (
+              <CardGroup
+                key={group.id}
+                group={group}
+                captureGroup={captureGroup}
+              />
+            );
+          })
+        ) : (
+          displayGroup.map((group) => {
+            return (
+              <CardGroup
+                key={group.id}
+                group={group}
+                captureGroup={captureGroup}
+              />
+            );
+          })
+        )}
+      </CardsContainer>
+      <PageButtons>
+        <Button white>
+          <FiChevronLeft size={20} />
+        </Button>
+        <span>1</span>
+        <Button white>
+          <FiChevronRight size={20} />
+        </Button>
+      </PageButtons>
+      {openAddNewGroup && <ModalGroupAddEdit/>}
+      {openAllActivities && <Activities capturedGroup={capturedGroup} />}
+      {openAllGoals && <ModalShowAllGoals capturedGroup={capturedGroup} />}
+    </Container>
   );
 };
 
-export default Routes;
+export default YourGroups;

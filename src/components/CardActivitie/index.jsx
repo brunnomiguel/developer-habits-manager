c49eb4106c2b0b8690956574f34e0201dc44f80c
Brunno Miguel <miguel.brunno06@gmail.com>
2022-06-30 22:25:55 -0300

    feat: edit and remove activities functions.

--- src/components/CardActivitie/index.jsx
+++ src/components/CardActivitie/index.jsx
@@ -1,9 +1,18 @@
 import { Container, CardHeader, DateTime, Buttons } from "./styles";
 import { FiEdit, FiDelete } from "react-icons/fi";
 import Button from "../Button";
+import { useContext } from "react";
+import { ActivitiesContext } from "../../providers/Activities";
+import { ModalContext } from "../../providers/Modal";
 
-const CardActivitie = ({ activity }) => {
-  const { title, realization_time } = activity;
+const CardActivitie = ({ activity, capturedGroupId, captureActivity }) => {
+  const { id, title, realization_time } = activity;
+  const { deleteActivity } = useContext(ActivitiesContext);
+  const { setOpenEditActivity } = useContext(ModalContext);
+
+  const handleClickFinish = () => {
+    deleteActivity(id, capturedGroupId);
+  };
 
   return (
     <Container>
@@ -15,10 +24,16 @@ const CardActivitie = ({ activity }) => {
         <p>{realization_time}</p>
       </DateTime>
       <Buttons>
-        <Button white>
+        <Button
+          white
+          onClick={() => {
+            setOpenEditActivity(true);
+            captureActivity(id);
+          }}
+        >
           <FiEdit size={20} />
         </Button>
-        <Button white>
+        <Button white onClick={() => handleClickFinish()}>
           <FiDelete size={20} />
         </Button>
       </Buttons>
